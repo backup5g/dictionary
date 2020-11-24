77729103a050a0352e9223ff66f23035eb214127
Diego Eiras <eiras.lucio@gmail.com>
2020-11-23 22:46:53 -0300

    📦 ADD: Dictionary screen layout

--- src/pages/Dictionary/index.js
+++ src/pages/Dictionary/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
-import styles from './styles';
+import letters from '../../mocks/letters';
+
+import Header from '../../components/Header';
+import WordsList from '../../components/WordsList';
+
+import { 
+  Container, 
+  Content,
+  LettersContainer,
+  Letter,
+  Label,
+  Helper,
+} from './styles';
 
 export default function Dictionary() {
+  const [selected, setSelected] = useState();
+
   return (
-    <View style={styles.container}>
-      <Text>Dictionary</Text>
-    </View>
+    <Container>
+      <Header screenTitle="Dicionário" />
+
+      <LettersContainer 
+        horizontal 
+        showsHorizontalScrollIndicator={false}
+      >
+        {letters.map((letter, index) => (
+          <Letter 
+            key={index}
+            $hasSelected={letter === selected}
+            onPress={() => setSelected(letter)}
+          >
+            <Label $hasSelected={selected === letter}>{letter}</Label>
+          </Letter>
+        ))}
+
+        <Helper />
+      </LettersContainer>
+
+      <Content>
+        <WordsList title="Todas as palavras" />
+      </Content>
+    </Container>
   );
 }
--- src/pages/Dictionary/styles.js
+++ src/pages/Dictionary/styles.js
@@ -1,5 +1,45 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled.ScrollView`
+  background: #FAFAFC;
 `;
+
+export const Content = styled.View`
+  position: relative;
+
+  flex: 1;
+  align-items: center;
+
+  padding: 0 32px 16px 32px;
+`;
+
+export const LettersContainer = styled.ScrollView`
+  margin-top: 48px;
+  padding: 0 32px;
+`;
+
+export const Letter = styled.TouchableOpacity`
+  width: 48px;
+  height: 48px;
+
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid #E4E6EA;
+  border-radius: 5px;
+
+  background: ${({ $hasSelected }) => $hasSelected ? '#2D8EFF' : '#F5F6F8'};
+
+  margin-right: 16px;
+`;
+
+export const Label = styled.Text`
+  color: ${({ $hasSelected }) => $hasSelected ? '#fff' : '#8694A8'};
+  font-family: 'Inter_700Bold';
+  font-size: 20px;
+`;
+
+export const Helper = styled.View`
+  width: 48px;
+  height: 48px;
+`
